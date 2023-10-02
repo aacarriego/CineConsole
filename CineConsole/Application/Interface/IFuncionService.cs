ad958021452e3a2d8e1f96f80b2ff96f3ea27ce6
Ariel Carriego <aa.carriego@gmail.com>
2023-10-02 17:58:21 -0300

    primer commmit correcion (sin andar)

---
+++ CineConsole/Application/Interface/IFuncionService.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interface
+{
+    public interface IFuncionService
+    {
+        void CreateFuncion(Funcion nuevaFuncion);
+
+        List<Funcion> GetAllFunciones();
+
+        List<Pelicula> GetAllPeliculas();
+
+        List<Sala> GetAllSalas();
+
+        List<Funcion> GetFuncionesPorFechaYPelicula(DateTime? fecha, string tituloPelicula);
+
+        List<Funcion> GetFuncionesPorFecha(DateTime fecha);
+
+        List<Funcion> GetFuncionesPorPelicula(int peliculaId);
+    }
+}
